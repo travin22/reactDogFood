65352c3c0f5f50bc2357e82732bd492ad54a762e
travin22 <travin-dima22@mail.ru>
2023-01-26 09:54:14 +0300

    Добавил изменения в кнопку вход. Когда в системе, вместо нее горит кнопка выйти.

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,8 +1,21 @@
 import { Link } from 'react-router-dom'
+import { useContext } from 'react'
+import { QueryClientProvider, useQueryClient } from '@tanstack/react-query'
+import { AppContext, AppSetContext } from '../context/AppContextProvider'
 import headerStyles from './header.module.css'
 import logo from '../images/DOGFOOD.png'
 
 export function Header() {
+  const { token } = useContext(AppContext)
+  const { setToken, setUserID } = useContext(AppSetContext)
+  const { clearClient } = useQueryClient(QueryClientProvider)
+
+  function logoutHandler() {
+    setToken('')
+    setUserID('')
+    setTimeout(clearClient)
+  }
+
   return (
     <header className={headerStyles.wr}>
       <nav>
@@ -10,15 +23,34 @@ export function Header() {
           <li>
             <Link to="/"><img src={logo} alt="DogFood" className={headerStyles.logo} /></Link>
           </li>
+          {token ? (
+            <li>
+              <Link
+                to="/signin"
+                className={headerStyles.link}
+              >
+                <button onClick={logoutHandler} type="button" className={headerStyles.btn}>
+                  Выйти
+                </button>
+              </Link>
+            </li>
+          ) : (
+            <li>
+              <Link
+                to="/signin"
+                className={headerStyles.link}
+              >
+                <button onClick={logoutHandler} type="button" className={headerStyles.btn}>
+                  Войти
+                </button>
+              </Link>
+            </li>
+          )}
           <li>
-            <Link to="/signin" className={headerStyles.link}>
-              <button type="button" className={headerStyles.btn}>
-                Войти
-              </button>
-            </Link>
-          </li>
-          <li>
-            <Link to="/signup" className={headerStyles.link}>
+            <Link
+              to="/signup"
+              className={headerStyles.link}
+            >
               <button type="button" className={headerStyles.btn}>
                 Регистрация
               </button>
